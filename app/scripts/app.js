712d1f3497d30598da7ad74c18a4b8646d3da2a5
Maya Lekova <apokalyptra@gmail.com>
2014-09-07 22:10:10 +0300

    Initial commit: Empty Yeoman app with AngularFire & Simple Login

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('fairyTreeAdminApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase',
+  'angularfire.firebase',
+  'angularfire.login',
+  'simpleLoginTools'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'MainCtrl'
+      })
+      .when('/login', {
+        authRequired: false, // if true, must log in before viewing this page
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fairyTreeAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
